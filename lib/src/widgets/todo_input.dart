90ff0bfb7acdc4cc1332d6b3eaee6fbeb49c8de7
Takafumi Umemoto <umecch@gmail.com>
2018-10-26 02:13:02 +0900

    Todo追加のためのボタンを配置

--- lib/src/widgets/todo_input.dart
+++ lib/src/widgets/todo_input.dart
@@ -3,6 +3,26 @@ import 'package:flutter/material.dart';
 class TodoInput extends StatelessWidget {
 
   Widget build(context) {
-    return TextFormField();
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextFormField()
+          ),
+          FlatButton(
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            color: Colors.blue,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            onPressed: () {
+
+            },
+          )
+        ],
+      ),
+    );
   }
 }
